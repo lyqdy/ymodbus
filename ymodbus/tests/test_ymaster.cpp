4e357a39d0bef3b6b29205dc500e234d973ecefc
lyqdy <53088228+lyqdy@users.noreply.github.com>
2019-07-20 11:47:28 +0800

    upload tests.

---
+++ ymodbus/tests/test_ymaster.cpp
@@ -0,0 +1,248 @@
+﻿/**
+* ymodbus
+* Copyright © 2019-2019 liuyongqing<lyqdy1@163.com>
+* v1.0.1 2019.05.04
+*/
+#include "ymblog.h"
+
+#include "ymod/ymbutils.h"
+#include "ymod/ymbtask.h"
+
+#include "ymod/master/ymbmaster.h"
+#include "ymod/master/ymaster.h"
+
+#include <iomanip>
+
+void LOG_Init(char *prog) {}
+void LOG_Fini(void) {}
+
+using namespace YModbus;
+
+template<typename T>
+class A
+{
+public:
+	void f(uint8_t sid, uint16_t startreg, T val)
+	{
+		YMB_DEBUG("AsyncRead val = %x(%u)\n", val, val);
+	}
+};
+
+using namespace std::placeholders;
+
+int main(int argc, char *argv[])
+{
+	LOG_Init(argv[0]);
+	LOG(INFO) << "test master enter" << std::endl;
+
+	Master tcpmaster("127.0.0.1", 5502, TCP, TASK);
+	TMaster<MRtu, TcpConnect> rtumaster("127.0.0.1", 5506, TASK);
+	TMaster<MRtu, TcpConnect> rtu2master("127.0.0.1", 5503, TASK);
+	TMaster<MNet, UdpConnect> udpmaster("127.0.0.1", 5508, TASK);
+
+	rtumaster.SetReadTimeout(1000);
+	tcpmaster.SetReadTimeout(500);
+	udpmaster.SetReadTimeout(500);
+
+	Task::LetUsGo();
+
+	uint8_t buf[BUFSIZ];
+	int len;
+
+	for (int i = 0; i < 500; i++)
+	{
+		std::shared_ptr<A<uint16_t>> a = std::make_shared<A<uint16_t>>();
+		udpmaster.AsyncRead<uint16_t>(1, 1, std::bind(&A<uint16_t>::f, a, _1, _2, _3));
+
+		std::shared_ptr<A<uint32_t>> b = std::make_shared<A<uint32_t>>();
+		udpmaster.AsyncRead<uint32_t>(1, 1, std::bind(&A<uint32_t>::f, b, _1, _2, _3));
+	}
+
+	udpmaster.WaitAsynReader();
+
+
+	udpmaster.SetByteOrder(BOR_1234);
+	{
+		uint16_t val = 0;
+		udpmaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_1234= %x(%u)\n", val, val);
+	}
+
+	{
+		uint32_t val = 0;
+		udpmaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_1234= %x(%lu)\n", val, val);
+	}
+
+	{
+		uint64_t val = 0;
+		udpmaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_1234= %llx(%llu)\n", val, val);
+	}
+
+	rtumaster.SetByteOrder(BOR_1234);
+	{
+		uint16_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_1234= %x(%u)\n", val, val);
+	}
+
+	{
+		uint32_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_1234= %x(%lu)\n", val, val);
+	}
+
+	{
+		uint64_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_1234= %llx(%llu)\n", val, val);
+	}
+
+	rtumaster.SetByteOrder(BOR_3412);
+	{
+		uint16_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_3412= %x(%u)\n", val, val);
+	}
+
+	{
+		uint32_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_3412= %x(%lu)\n", val, val);
+	}
+
+	{
+		uint64_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_3412= %llx(%llu)\n", val, val);
+	}
+
+	rtumaster.SetByteOrder(BOR_4321);
+	{
+		uint16_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_4321= %x(%u)\n", val, val);
+	}
+
+	{
+		uint32_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_4321= %x(%lu)\n", val, val);
+	}
+
+	{
+		uint64_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_4321= %llx(%llu)\n", val, val);
+	}
+
+	rtumaster.SetByteOrder(BOR_2143);
+	{
+		uint16_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_2143= %x(%u)\n", val, val);
+	}
+
+	{
+		uint32_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_2143= %x(%lu)\n", val, val);
+	}
+
+	{
+		uint64_t val = 0;
+		rtumaster.ReadValue(1, 1, val);
+		YMB_DEBUG("ReadValue BOR_2143= %llx(%llu)\n", val, val);
+	}
+
+	exit(0);
+
+	len = tcpmaster.ReadHoldingRegisters(1, 1, 1, buf, sizeof(buf));
+	if (len > 0) {
+		YMB_HEXDUMP(buf, len, "tcpReadHoldingRegisters: ");
+	}
+	YMB_ASSERT(len == 2);
+
+	uint32_t rcount = 0;
+	uint32_t ecount = 0;
+
+	do {
+		rcount++;
+		len = tcpmaster.ReadHoldingRegisters(1, 1, 10, buf, sizeof(buf));
+		if (len > 0) {
+			YMB_HEXDUMP0(buf, len, "tcpReadHoldingRegisters: ");
+		}
+		else {
+			ecount++;
+		}
+		YMB_DEBUG("REQ:%u, ERR:%u\n", rcount, ecount);
+	}  while (rcount < 10000);
+
+	len = tcpmaster.ReadHoldingRegisters(1, 1, 100, buf, sizeof(buf));
+	if (len > 0) {
+		YMB_HEXDUMP(buf, len, "tcpReadHoldingRegisters: ");
+	}
+	YMB_ASSERT(len == 200);
+		
+
+	len = rtumaster.ReadHoldingRegisters(1, 1, 1, buf, sizeof(buf));
+	if (len > 0) {
+		YMB_HEXDUMP(buf, len, "rtuReadHoldingRegisters: ");
+	}
+	YMB_ASSERT(len == 2);
+
+	len = rtumaster.ReadHoldingRegisters(1, 1, 10, buf, sizeof(buf));
+	if (len > 0) {
+		YMB_HEXDUMP(buf, len, "rtuReadHoldingRegisters: ");
+	}
+	YMB_ASSERT(len == 20);
+
+	rcount = 0;
+	ecount = 0;
+	do {
+		rcount++;
+		len = rtumaster.ReadHoldingRegisters(1, 1, 10, buf, sizeof(buf));
+		if (len > 0) {
+			YMB_HEXDUMP0(buf, len, "tcpReadHoldingRegisters: ");
+		}
+		else {
+			ecount++;
+		}
+		YMB_DEBUG("REQ:%u, ERR:%u\n", rcount, ecount);
+	} while (rcount < 10000);
+
+	len = rtumaster.ReadHoldingRegisters(1, 1, 100, buf, sizeof(buf));
+	if (len > 0) {
+		YMB_HEXDUMP(buf, len, "rtuReadHoldingRegisters: ");
+	}
+	YMB_ASSERT(len == 200);
+
+	len = rtu2master.ReadHoldingRegisters(1, 1, 1, buf, sizeof(buf));
+	if (len > 0) {
+		YMB_HEXDUMP(buf, len, "rtuReadHoldingRegisters: ");
+	}
+	YMB_ASSERT(len == 2);
+
+	rcount = 0;
+	ecount = 0;
+	do {
+		rcount++;
+		len = rtu2master.ReadHoldingRegisters(1, 1, 10, buf, sizeof(buf));
+		if (len > 0) {
+			YMB_HEXDUMP0(buf, len, "tcpReadHoldingRegisters: ");
+		}
+		else {
+			ecount++;
+		}
+		YMB_DEBUG("REQ:%u, ERR:%u\n", rcount, ecount);
+	} while (rcount < 10000);
+
+	Task::LetUsDone();
+
+	LOG(INFO) << "test master quit" << std::endl;
+	LOG_Fini();
+
+	return 0;
+}
+
---
+++ ymodbus/tests/test_yslave.cpp
@@ -0,0 +1,146 @@
+﻿/**
+* ymodbus
+* Copyright © 2019-2019 liuyongqing<lyqdy1@163.com>
+* v1.0.1 2019.05.04
+*/
+// test_yslave.cpp 
+//
+#include "ymod/slave/ymbslave.h"
+#include "ymod/slave/yslave.h"
+
+namespace YModbus {
+
+class Player : public IPlayer
+{
+public:
+	//return: >= 0, bytes of data to return;
+	//return: < 0, errorcode of exception
+	virtual int ReadCoils(uint8_t sid,
+		uint16_t reg, uint16_t num, uint8_t *buf, size_t bufsiz)
+	{
+		*buf++ = 0x01;
+		*buf++ = 0x02;
+		return num / 8 + ((num % 8) != 0 ? 1 : 0);
+	}
+	virtual int ReadDiscreteInputs(uint8_t sid,
+		uint16_t reg, uint16_t num, uint8_t *buf, size_t bufsiz)
+	{
+		*buf++ = 0x01;
+		*buf++ = 0x02;
+		return num / 8 + ((num % 8) != 0 ? 1 : 0);
+	}
+	virtual int ReadInputRegisters(uint8_t sid,
+		uint16_t reg, uint16_t num, uint8_t *buf, size_t bufsiz)
+	{
+		*buf++ = 0x01;
+		*buf++ = 0x02;
+		return num * 2;
+	}
+	virtual int ReadHoldingRegisters(uint8_t sid,
+		uint16_t reg, uint16_t num, uint8_t *buf, size_t bufsiz)
+	{
+		*buf++ = 0x01;
+		*buf++ = 0x02;
+		*buf++ = 0x03;
+		*buf++ = 0x04;
+		*buf++ = 0x05;
+		*buf++ = 0x06;
+		*buf++ = 0x07;
+		*buf++ = 0x08;
+		return num * 2;
+	}
+
+	//return: >= 0, OK; 
+	//return: < 0, errorcode of exception
+	virtual int WriteSingleCoil(uint8_t sid,
+		uint16_t reg, bool onoff)
+	{
+		return 0;
+	}
+	virtual int WriteCoils(uint8_t sid,
+		uint16_t reg, uint16_t num, const uint8_t *bits, uint8_t wbytes)
+	{
+		return 0;
+	}
+	virtual int WriteSingleRegister(uint8_t sid,
+		uint16_t reg, uint16_t value)
+	{
+		return 0;
+	}
+	virtual int WriteRegisters(uint8_t sid,
+		uint16_t reg, uint16_t num, const uint8_t *values, uint8_t wbytes)
+	{
+		return 0;
+	}
+	virtual int MaskWriteRegisters(uint8_t sid,
+		uint16_t reg, uint16_t andmask, uint16_t ormask)
+	{
+		return 0;
+	}
+
+	//return: >= 0, bytes of data to return
+	//return: < 0,  errorcode of exception
+	virtual int WriteReadRegisters(uint8_t sid,
+		uint16_t wreg, uint16_t wnum, const uint8_t *values, uint8_t wbytes,
+		uint16_t rreg, uint16_t rnum, uint8_t *buf, size_t bufsiz)
+	{
+		*buf++ = 0x01;
+		*buf++ = 0x02;
+		return rnum * 2;
+	}
+
+	//return: >= 0, OK
+	//return: < 0,  errorcode of exception
+	virtual int ReportSlaveId(uint8_t maxsid, uint8_t *buf, size_t bufsiz)
+	{
+		return -1;
+	}
+};
+
+} //namespace YModbus
+
+using namespace YModbus;
+
+int main()
+{
+	Slave slave1(5502, TCP, TASK);
+	Slave slave2(5503, TCPRTU, TASK);
+	Slave slave3(5504, TCPASCII, TASK);
+
+#ifdef WIN32
+	//Slave slave4("COM3", 256000, kSerParityNone, kSerStopbits1, RTU, TASK);
+#else
+	Slave slave4("/dev/ttyS9", 115200, kSerParityNone, kSerStopbits1, YMB_PROT_RTU, YMB_THR_POLL);
+#endif
+
+	TSlave<SNet, TcpListener, Player> slave5(5505, TASK);
+	TSlave<SRtu, TcpListener, Player> slave6(5506, TASK);
+	TSlave<SAscii, TcpListener, Player> slave7(5507, TASK);
+
+	Slave slave8(5508, UDP, TASK);
+
+	slave1.SetPlayer(std::make_shared<YModbus::Player>());
+	slave2.SetPlayer(std::make_shared<YModbus::Player>());
+	slave3.SetPlayer(std::make_shared<YModbus::Player>());
+	//slave4.SetPlayer(std::make_shared<YModbus::Player>());
+	slave5.SetPlayer(std::make_shared<YModbus::Player>());
+	slave6.SetPlayer(std::make_shared<YModbus::Player>());
+	slave7.SetPlayer(std::make_shared<YModbus::Player>());
+	slave8.SetPlayer(std::make_shared<YModbus::Player>());
+
+	slave1.Startup();
+	slave2.Startup();
+	slave3.Startup();
+	//slave4.Startup();
+	slave5.Startup();
+	slave6.Startup();
+	slave7.Startup();
+	slave8.Startup();
+
+	Task::LetUsGo();
+
+	while (1);
+
+    return 0;
+}
+
